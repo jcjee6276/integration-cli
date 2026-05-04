c86fde38e8bbab1879581a528633ccb9e6081894
jcjeon <lifemeans@naver.com>
2026-05-04 17:22:40 +0900

    fix: eslint 경로 인식 문제 수정

--- apps/server/eslint.config.mjs
+++ apps/server/eslint.config.mjs
@@ -1,3 +1,20 @@
+/*
+ * @Author: 전지창
+ * @Date: 2026-05-04 17:17:33
+ * @LastEditTime: 2026-05-04 17:21:47
+ * @LastEditors: 전지창
+ * @Description:
+ */
 import nestConfig from "@ji/eslint-config/nest";
 
-export default nestConfig;
+export default [
+  ...nestConfig,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: path.join(import.meta.dirname, ".."),
+      },
+    },
+  },
+];
